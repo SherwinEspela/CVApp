248d694d3dcbafea6a65fe98867391978a442041
Sherwin Espela <sherwin.espela@gmail.com>
2019-10-21 21:11:45 -0700

    [master] - some visual enhancements, finetuning layout - adding Header Cell

---
+++ CVApp/CVApp/View/CustomCells/HeaderCell.swift
@@ -0,0 +1,24 @@
+//
+//  HeaderCell.swift
+//  CVApp
+//
+//  Created by Sherwin Espela on 2019-10-21.
+//  Copyright © 2019 cybermash. All rights reserved.
+//
+
+import UIKit
+
+class HeaderCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
